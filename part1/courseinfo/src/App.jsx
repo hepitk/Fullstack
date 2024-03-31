bd4508b0d185fb0f868d705c217420e849634257
hepitk <heino.pitkanen@helsinki.fi>
2024-03-31 18:48:13 +0300

    did exercises 1.1-1.5

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+    const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total total={total} />
+        </div>
+    )
+}
+
+const Header = ({ course }) => {
+    return <h1>{course}</h1>
+}
+
+const Part = ({ name, exercises }) => {
+    return (
+        <p>
+            {name} {exercises}
+        </p>
+    );
+};
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            <Part name={parts[0].name} exercises={parts[0].exercises} />
+            <Part name={parts[1].name} exercises={parts[1].exercises} />
+            <Part name={parts[2].name} exercises={parts[2].exercises} />
+        </div>
+    )
+}
+
+const Total = ({ total }) => {  
+    return (
+    <div>
+        <p>Number of exercises {total}</p>
+    </div>
+    )
+}
+
+export default App
